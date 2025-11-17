d36d6b6dd7898a55c04d83e0c40792cdf1f8b558
Erick Muller <erick@em.pro.br>
2025-11-17 19:25:36 -0300

    feat: criando consumer - inicial

---
+++ ms-entrada-pedidos-proc/src/main/java/br/dev/empgenent/entradapedidos/MsEntradaPedidosProcApplication.java
@@ -0,0 +1,13 @@
+package br.dev.empgenent.entradapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEntradaPedidosProcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEntradaPedidosProcApplication.class, args);
+	}
+
+}
---
+++ ms-entrada-pedidos-proc/src/test/java/br/dev/empgenent/entradapedidos/MsEntradaPedidosProcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.empgenent.entradapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEntradaPedidosProcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
