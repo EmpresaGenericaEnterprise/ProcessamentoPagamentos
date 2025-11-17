14f03631bf9c34a4406220be789f450f6f65a092
Erick Muller <erick@em.pro.br>
2025-11-17 20:24:45 -0300

    feat: publicando no kafka

--- ms-entrada-pedidos-proc/src/main/java/br/dev/empgenent/entradapedidos/service/SubscribeService.java
+++ ms-entrada-pedidos-proc/src/main/java/br/dev/empgenent/entradapedidos/service/SubscribeService.java
@@ -2,16 +2,27 @@ package br.dev.empgenent.entradapedidos.service;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SubscribeService {
 
+    @Value("${kafka.topic.pedidos.nome}")
+    String topicName;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @RabbitListener(queues = "${broker.queue.pedidos.nome}")
     public void receiveMessage(Message message) {
         String body = new String(message.getBody());
         System.out.println("Received message: " + body);
+
+        // kafkaTemplate.send("entrada-pedidos", body);
+        kafkaTemplate.send("entrada-pedidos", "my-key", body);
     }
 
 }
